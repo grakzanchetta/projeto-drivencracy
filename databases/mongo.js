8057b0ddbd69596fd185fecc9a082ad4f1239cc4
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-12 12:56:09 -0300

    pequena correção numa das variaveis

--- databases/mongo.js
+++ databases/mongo.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
     db = mongoClient.db(process.env.DB_PROJECT);
-    console.log(`Database running on port ${process.env.MONGO_URL}`)
+    console.log(`Database running on port ${process.env.MONGO_URI}`)
 });
 
 export default { db } ;
