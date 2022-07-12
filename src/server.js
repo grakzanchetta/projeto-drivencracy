e6fc1b7a47fdc1b24e4f2b76a50de2ff4c8b150c
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-12 13:01:51 -0300

    chore: adjust code for deploy on heroku

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 const server = express();
 server.use(express.json());
 
+//this is a comment
 server.listen(process.env.PORT, () => 
     console.log (`Server running on port ${process.env.PORT}`)
 )
